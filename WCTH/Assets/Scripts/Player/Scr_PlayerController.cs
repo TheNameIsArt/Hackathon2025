aa19f469850013e4e218b0d2efcb0150062cb0e0
MathiasPHA <Mathias.p.h.andersen@gmail.com>
2025-04-29 16:42:18 +0200

    PLAYA

---
+++ WCTH/Assets/Scripts/Player/Scr_PlayerController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Scr_PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private InputDevice inputDevice;
+    private Vector2 moveInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        InputSystem.onActionChange += OnActionChange;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+        Animate();
+    }
+    private void OnActionChange(object obj, InputActionChange change)
+    {
+        if (change == InputActionChange.ActionPerformed)
+        {
+            var inputAction = (InputAction)obj;
+            var lastControl = inputAction.activeControl;
+            inputDevice = lastControl.device;
+        }
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    void Animate()
+    {
+        if (moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (moveInput.x != 0 || moveInput.y != 0)
+        {
+            animator.Play("Andy_Run");
+        }
+        else
+        {
+            animator.Play("Andy_Idle");
+        }
+    }
+}
