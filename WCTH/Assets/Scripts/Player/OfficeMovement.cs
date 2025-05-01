ac460e431f561946d7f01c6dea0076e8a84b6177
GustavNH <gustavnoerhaugesen@gmail.com>
2025-05-01 23:57:51 +0200

    lalala

--- WCTH/Assets/Scripts/Player/OfficeMovement.cs
+++ WCTH/Assets/Scripts/Player/OfficeMovement.cs
@@ -4,47 +4,68 @@ using Cinemachine;
 
 public class OfficeMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float speed;
-    public float input;
+
+    public InputActionAsset inputActions;
+
+    private InputAction m_moveActionGP;
+    private InputAction m_moveActionKB;
+
+    private Vector2 m_movement;
+    private Animator m_animator;
+    private Rigidbody2D m_Rigidbody;
+
+    public float moveSpeed = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnEnable()
     {
+        inputActions.FindActionMap("Office Movement").Enable();
+    }
 
+    private void OnDisable()
+    {
+        inputActions.FindActionMap("Office Movement").Disable();
+    }
+
+    private void Awake()
+    {
+        m_moveActionGP = InputSystem.actions.FindAction("Movement");
+        m_moveActionKB = InputSystem.actions.FindAction("klmMovement");
+
+        m_animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        input = Input.GetAxisRaw("Horizonal");
+        Animate();
     }
 
-    private void FixedUpdate()
+    void Sidescrolling(InputAction.CallbackContext context)
     {
-        rb.linearVelocity = new Vector2(input * speed, input * speed);
+        m_movement = context.ReadValue<Vector2>();
     }
-
-    /*
+    
     void Animate()
     {
-        if (moveInput.x > 0)
+        if (m_movement.x > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
         }
-        else if (moveInput.x < 0)
+        else if (m_movement.x < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        if (moveInput.x != 0 || moveInput.y != 0)
+        if (m_movement.x != 0 || m_movement.y != 0)
         {
-            animator.Play("Andy_Run");
+            m_animator.Play("Andy_Run");
         }
         else
         {
-            animator.Play("Andy_Idle");
+            m_animator.Play("Andy_Idle");
         }
-    }*/
+    }
 
 }
