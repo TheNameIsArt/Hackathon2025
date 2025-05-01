65eea3720aae8357f8d39b80ac4e549d3966a77d
Viktor <viktor.bergstroem@hotmail.com>
2025-05-01 14:59:33 +0200

    Carls MG done
    Potentialy add assets

--- WCTH/Assets/Scripts/Carl_Minigame/CarlsMinigameManager.cs
+++ WCTH/Assets/Scripts/Carl_Minigame/CarlsMinigameManager.cs
@@ -6,12 +6,15 @@ public class CarlsMinigameManager : MonoBehaviour
 {
     private int lives = 3;
     private int gameOverLifeAmount = 0;
+    private int score;
+    private int scoreToWin = 25;
     private float timePlayed;
     public GameObject gameoverUI;
+    public GameObject tryAgainUI;
     public TextMeshProUGUI livesText;
     public TextMeshProUGUI infoText;
     public TextMeshProUGUI scoreText;
-
+    public AudioSource damageSound;
 
     void Awake()
     {
@@ -29,13 +32,17 @@ public class CarlsMinigameManager : MonoBehaviour
         
         timePlayed += Time.deltaTime;
         scoreText.text ="Score: " + Mathf.FloorToInt(timePlayed); //Mathf runder til hele tal
+        score = + Mathf.FloorToInt(timePlayed);
         
         if (lives <= gameOverLifeAmount)
         {
             GameOver();
         }
 
-
+        if (score >= scoreToWin)
+        {
+            Win();
+        }
 
     }
 
@@ -46,6 +53,7 @@ public class CarlsMinigameManager : MonoBehaviour
             lives --;
             Debug.Log("Life lost!");
             collision.gameObject.SetActive(false);
+            damageSound.PlayOneShot(damageSound.clip);
 
         }
     }
@@ -55,6 +63,14 @@ public class CarlsMinigameManager : MonoBehaviour
         Debug.Log("game over!");
         Time.timeScale = 0f; //Game gets paused
         gameoverUI.SetActive(true);
+    }
+
+    void Win()
+    {
+        Debug.Log("You Win!");
+        Time.timeScale = 0f; //Game gets paused
+        //Indsæt Lil Carl der siger "What!?!?!"s
+         tryAgainUI.SetActive(true);
     }
 
     public void TryAgain()
